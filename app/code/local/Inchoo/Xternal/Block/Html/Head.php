3897bc0529965d3ee7b1a643ce2577e9cb1d7b71
root <root@hosting>
2014-09-15 14:51:08 +0000

    removed example styles, corrected order (js files should be after css files, faster page loading)

--- app/code/local/Inchoo/Xternal/Block/Html/Head.php
+++ app/code/local/Inchoo/Xternal/Block/Html/Head.php
@@ -104,13 +104,88 @@ class Inchoo_Xternal_Block_Html_Head extends Mage_Page_Block_Html_Head
                 break;
             
            	case 'external_js':
-                $lines[$itemIf]['other'][] = sprintf('<script type="text/javascript" src="%s" %s></script>', $href, $params);
+                $lines[$itemIf]['other_js'][] = sprintf('<script type="text/javascript" src="%s" %s></script>', $href, $params);
                 break;
                             
           	case 'external_css':
-                $lines[$itemIf]['other'][] = sprintf('<link rel="stylesheet" type="text/css" href="%s" %s/>', $href, $params);
+                $lines[$itemIf]['other_css'][] = sprintf('<link rel="stylesheet" type="text/css" href="%s" %s/>', $href, $params);
                 break;
         }
     }
 
+    /**
+     * Get HEAD HTML with CSS/JS/RSS definitions
+     * (actually it also renders other elements, TODO: fix it up or rename this method)
+     *
+     * @return string
+     */
+    public function getCssJsHtml()
+    {
+        // separate items by types
+        $lines  = array();
+
+        foreach ($this->_data['items'] as $item) {
+            if (!is_null($item['cond']) && !$this->getData($item['cond']) || !isset($item['name'])) {
+                continue;
+            }
+            $if     = !empty($item['if']) ? $item['if'] : '';
+            $params = !empty($item['params']) ? $item['params'] : '';
+            switch ($item['type']) {
+                case 'js':        // js/*.js
+                case 'skin_js':   // skin/*/*.js
+                case 'js_css':    // js/*.css
+                case 'skin_css':  // skin/*/*.css
+                    $lines[$if][$item['type']][$params][$item['name']] = $item['name'];
+                    break;
+                default:
+                    $this->_separateOtherHtmlHeadElements($lines, $if, $item['type'], $params, $item['name'], $item);
+                    break;
+            }
+        }
+
+        // prepare HTML
+        $shouldMergeJs = Mage::getStoreConfigFlag('dev/js/merge_files');
+        $shouldMergeCss = Mage::getStoreConfigFlag('dev/css/merge_css_files');
+        $html   = '';
+        foreach ($lines as $if => $items) {
+            if (empty($items)) {
+                continue;
+            }
+            if (!empty($if)) {
+                $html .= '<!--[if '.$if.']>'."\n";
+            }
+
+            // other stuff
+            if (!empty($items['other'])) {
+                $html .= $this->_prepareOtherHtmlHeadElements($items['other']) . "\n";
+            }
+
+            if (!empty($items['other_css'])) {
+                $html .= $this->_prepareOtherHtmlHeadElements($items['other_css']) . "\n";
+            }
+
+            // static and skin css
+            $html .= $this->_prepareStaticAndSkinElements('<link rel="stylesheet" type="text/css" href="%s"%s />' . "\n",
+                empty($items['js_css']) ? array() : $items['js_css'],
+                empty($items['skin_css']) ? array() : $items['skin_css'],
+                $shouldMergeCss ? array(Mage::getDesign(), 'getMergedCssUrl') : null
+            );
+
+            if (!empty($items['other_js'])) {
+                $html .= $this->_prepareOtherHtmlHeadElements($items['other_js']) . "\n";
+            }
+
+            // static and skin javascripts
+            $html .= $this->_prepareStaticAndSkinElements('<script type="text/javascript" src="%s"%s></script>' . "\n",
+                empty($items['js']) ? array() : $items['js'],
+                empty($items['skin_js']) ? array() : $items['skin_js'],
+                $shouldMergeJs ? array(Mage::getDesign(), 'getMergedJsUrl') : null
+            );
+
+            if (!empty($if)) {
+                $html .= '<![endif]-->'."\n";
+            }
+        }
+        return $html;
+    }
 }
